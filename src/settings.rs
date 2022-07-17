fc400b7c7fba9aa933c4bf37193fd8c93762d3fd
Ted Steen <ted.steen@gmail.com>
2022-07-17 16:50:20 +0300

    cleanups

--- src/settings.rs
+++ src/settings.rs
@@ -13,19 +13,16 @@ pub(crate) struct Settings {
 
 use winit::event::VirtualKeyCode::*;
 
-pub(crate) const DEFAULT_P1_INPUT_ID: &str = "00-keyboard-1";
-pub(crate) const DEFAULT_P2_INPUT_ID: &str = "00-keyboard-2";
-
 impl Settings {
     fn default_p1_conf() -> InputConfiguration {
-        InputConfiguration { name: "Keyboard mapping #1".to_string(), id: DEFAULT_P1_INPUT_ID.to_string(), disconnected: false, kind: InputConfigurationKind::Keyboard(JoypadKeyboardKeyMap {
+        InputConfiguration { name: "Keyboard mapping #1".to_string(), id: "00-keyboard-1".to_string(), disconnected: false, kind: InputConfigurationKind::Keyboard(JoypadKeyboardKeyMap {
             up: Some(Up), down: Some(Down), left: Some(Left), right: Some(Right),
             start: Some(Return), select: Some(RShift),
             b: Some(Key1), a: Some(Key2)
         })}
     }
     fn default_p2_conf() -> InputConfiguration {
-        InputConfiguration { name: "Keyboard mapping #2".to_string(), id: DEFAULT_P2_INPUT_ID.to_string(), disconnected: false, kind: InputConfigurationKind::Keyboard(JoypadKeyboardKeyMap {
+        InputConfiguration { name: "Keyboard mapping #2".to_string(), id: "00-keyboard-2".to_string(), disconnected: false, kind: InputConfigurationKind::Keyboard(JoypadKeyboardKeyMap {
             up: Some(W), down: Some(S), left: Some(A), right: Some(D),
             start: Some(Key9), select: Some(Key0),
             b: Some(LAlt), a: Some(LControl)
@@ -33,13 +30,12 @@ impl Settings {
     }
 
     fn get_config(&mut self, player: usize, default: InputConfiguration) -> &mut InputConfiguration {
-        let default_id = default.id.clone();
-        let mut id = self.selected_inputs[player].get_or_insert(default_id.clone()).clone();
+        let mut id = self.selected_inputs[player].get_or_insert(default.id.clone()).clone();
 
         //Make sure we switch to default if it's disconnected.
         if let Some(config) = self.input_configurations.get(&id) {
             if config.disconnected {
-                id = default_id;
+                id = default.id.clone();
                 self.selected_inputs[player] = Some(id.clone());
             }
         }
