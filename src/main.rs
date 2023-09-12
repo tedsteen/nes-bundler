c48d62a0db60356e99e6776068578d2d909991ae
Ted Steen <ted.steen@gmail.com>
2023-09-12 10:53:34 +0300

    Windows file logging (#51)
    * Windows file logging
    
    * Added missing use for windows
    
    * Files are open in shared mode by default

--- src/main.rs
+++ src/main.rs
@@ -149,7 +149,31 @@ pub struct BuildConfiguration {
 }
 
 fn main() -> Result<()> {
-    env_logger::init();
+    #[cfg(windows)]
+    {
+        match std::fs::OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open("nes-bundler-log.txt")
+        {
+            Ok(log_file) => {
+                env_logger::Builder::from_env(env_logger::Env::default())
+                    .target(env_logger::Target::Pipe(Box::new(log_file)))
+                    .init();
+            }
+            Err(e) => {
+                eprintln!("Could not open nes-bundler-log.txt for writing, {:?}", e);
+                env_logger::init();
+            }
+        }
+    }
+    #[cfg(not(windows))]
+    {
+        env_logger::init();
+    }
+
+    log::info!("nes-bundler starting!");
 
     // This is required for certain controllers to work on Windows without the
     // video subsystem enabled:
