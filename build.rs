d436e2a03b345b0986e8f665daf93ed9b3adf45e
Ted Steen <ted.steen@gmail.com>
2024-01-10 23:17:03 +0200

    static linking the MSVC runtime and target x86_64 instead of i686 (#69)
    * static linking the MSVC runtime
    
    * target windows x86_64 instead of i686
    
    * build audio strech code optimized for windows as well

--- build.rs
+++ build.rs
@@ -6,9 +6,15 @@ fn main() {
     println!("cargo:rerun-if-changed=src/audio/stretch/signalsmith-stretch-wrapper.hpp");
     println!("cargo:rerun-if-changed=src/audio/stretch/signalsmith-stretch-wrapper.cpp");
     println!("cargo:rerun-if-changed=src/audio/stretch/mod.rs");
-    cxx_build::bridge("src/audio/stretch/mod.rs")
+    let mut code = cxx_build::bridge("src/audio/stretch/mod.rs");
+    let code = code
         .file("src/audio/stretch/signalsmith-stretch-wrapper.cpp")
-        .flag_if_supported("-std=c++11")
-        .flag("-O3")
-        .compile("signalsmith-stretch");
+        .flag_if_supported("-std=c++11");
+
+    #[cfg(not(target_os = "windows"))]
+    code.flag("-O3");
+    #[cfg(target_os = "windows")]
+    code.flag("/O2");
+
+    code.compile("signalsmith-stretch");
 }
