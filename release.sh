a4566acf8e46413917c107ac232c8fb3355c33a1
Ted Steen <ted.steen@gmail.com>
2024-02-29 00:27:14 +0200

    use cargo release

--- release.sh
+++ release.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 VERSION=$1
-#if [[ $(git branch --show-current) == "master" ]]; then
-    cargo bump -g $VERSION
-    exit 0
-#else
-#    >&2 echo "Can only release from master"
-#fi
-echo "USAGE:"
-echo "    $0 [<version> | major | minor | patch]"
+cargo release $VERSION
