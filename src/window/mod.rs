f91871c36f7f941e3bc304b89206262dd08627ec
Ted Steen <ted.steen@gmail.com>
2024-03-12 09:52:27 +0200

    Fix windows icon (#104)
    * Use ico file for windows window title icon
    
    * update documentation

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -3,9 +3,7 @@ use std::{num::NonZeroU32, sync::Arc};
 use anyhow::Result;
 use egui::NumExt;
 use glutin::config::ConfigSurfaceTypes;
-use image::DynamicImage;
 use raw_window_handle::HasRawWindowHandle;
-use winit::window::Icon;
 
 use crate::input::keys::{KeyCode, Modifiers};
 mod winit_impl;
@@ -46,7 +44,6 @@ impl GlutinWindowContext {
     // preferably add android support at the same time.
     pub fn new(
         title: &str,
-        window_icon: Option<DynamicImage>,
         inner_size: Size,
         min_inner_size: Size,
         event_loop: &winit::event_loop::EventLoopWindowTarget<()>,
@@ -57,12 +54,6 @@ impl GlutinWindowContext {
 
         let winit_window_builder = winit::window::WindowBuilder::new()
             .with_resizable(true)
-            .with_window_icon(window_icon.map(|image| {
-                let image = image.into_rgba8();
-                let (width, height) = image.dimensions();
-                Icon::from_rgba(image.into_raw(), width, height).expect("Failed to open icon")
-            }))
-            //.with_disallow_hidpi(true)
             .with_inner_size(inner_size)
             .with_min_inner_size(min_inner_size)
             .with_title(title)
@@ -96,6 +87,53 @@ impl GlutinWindowContext {
 
         let raw_window_handle = window.as_ref().map(|w| w.raw_window_handle());
         log::debug!("raw window handle: {:?}", raw_window_handle);
+
+        #[cfg(windows)]
+        {
+            fn get_instance_handle() -> windows_sys::Win32::Foundation::HMODULE {
+                // Gets the instance handle by taking the address of the
+                // pseudo-variable created by the microsoft linker:
+                // https://devblogs.microsoft.com/oldnewthing/20041025-00/?p=37483
+
+                // This is preferred over GetModuleHandle(NULL) because it also works in DLLs:
+                // https://stackoverflow.com/questions/21718027/getmodulehandlenull-vs-hinstance
+
+                extern "C" {
+                    static __ImageBase:
+                        windows_sys::Win32::System::SystemServices::IMAGE_DOS_HEADER;
+                }
+
+                unsafe { &__ImageBase as *const _ as _ }
+            }
+
+            if let Some(raw_window_handle::RawWindowHandle::Win32(w)) = raw_window_handle {
+                use windows_sys::Win32::UI::WindowsAndMessaging::*;
+                let instance_handle = get_instance_handle();
+                log::debug!("Got instance handle: {:?}", instance_handle);
+                let icon_handle = unsafe {
+                    LoadIconW(
+                        instance_handle,
+                        1 as usize as windows_sys::core::PCWSTR, /* MAKEINTRESOURCEW */
+                    )
+                };
+                log::debug!("Got icon handle: {:?}", icon_handle);
+                if icon_handle != 0 {
+                    let window_handle = w.hwnd as isize;
+                    log::debug!("Got window handle: {:?}", icon_handle);
+                    for icon_type in [ICON_BIG, ICON_SMALL, ICON_SMALL2] {
+                        unsafe {
+                            SendMessageW(
+                                window_handle,
+                                WM_SETICON,
+                                icon_type.try_into().unwrap(),
+                                icon_handle,
+                            )
+                        };
+                    }
+                }
+            }
+        }
+
         let context_attributes =
             glutin::context::ContextAttributesBuilder::new().build(raw_window_handle);
         // by default, glutin will try to create a core opengl context. but, if it is not available, try to create a gl-es context using this fallback attributes
@@ -190,10 +228,9 @@ impl GlutinWindowContext {
 
 pub fn create_display(
     title: &str,
-    window_icon: Option<DynamicImage>,
     inner_size: Size,
     min_inner_size: Size,
     event_loop: &winit::event_loop::EventLoopWindowTarget<()>,
 ) -> Result<GlutinWindowContext> {
-    GlutinWindowContext::new(title, window_icon, inner_size, min_inner_size, event_loop)
+    GlutinWindowContext::new(title, inner_size, min_inner_size, event_loop)
 }
