59d4812c3f27cf1187afb88027fbfe5f17b261b9
Ted Steen <ted.steen@gmail.com>
2025-04-22 23:25:22 +0100

    Use filebin.net instead of bashupload.com

---
+++ build_scripts/upload.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+set -x
+if (( $# != 2 )); then
+  echo "Usage: upload <bin> <file>" >&2
+  exit 1
+fi
+bin="$1"
+file="$2"
+if [[ ! -f $file ]]; then
+  echo "Error: '$file' not found" >&2
+  exit 1
+fi
+
+filename=$(basename "$file")
+resp=$(curl -s -X POST \
+  --data-binary @"$file" \
+  -H "bin: $bin" \
+  -H "filename: $filename" \
+  https://filebin.net)
