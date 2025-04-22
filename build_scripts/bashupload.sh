3e866527aa090a423dfbff923f1ea18014d4bbfb
Ted Steen <ted.steen@gmail.com>
2025-04-22 20:29:08 +0100

    Fix workflow config and email sending (#150)
    * make sure to hide the configuration url
    
    * Fix config download (#149)
    
    * download artifact and encrypt it to be able to pass it to the matrix
    
    * fix version
    
    * re-enable workflow_dispatch
    
    * remove it again..
    
    * small encryption and filename fix
    
    * fix spaces in emails
    
    * Switch back to the previous encryption
    
    * make sure secrets are hidden
    
    * Use a github secret for the config zip
    
    * make sure we have the config ready before starting the build
    
    * Get pass gmail attachment block
    
    * fix typo
    
    * fix file prefixes so the can be emailed
    
    * use bashupload to get the files to email

---
+++ build_scripts/bashupload.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if (( $# != 1 )); then
+  echo "Usage: bashupload <file>" >&2
+  exit 1
+fi
+
+file="$1"
+if [[ ! -f $file ]]; then
+  echo "Error: '$file' not found" >&2
+  exit 1
+fi
+
+fname=$(basename "$file")
+
+response=$(curl --silent --show-error --upload-file "$file" "https://bashupload.com/$fname")
+
+url=$(awk '/wget/ { print $2; exit }' <<< "$response")
+
+if [[ -z "$url" ]]; then
+  echo "Upload failed: $response" >&2
+  exit 1
+fi
+
+echo "$url"
