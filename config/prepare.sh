9fb637c4a5a995950a218617c4b8582dc6c87fa5
Ted Steen <ted.steen@gmail.com>
2025-05-25 22:41:01 +0100

    warn if config.zip is already there in prepare.sh

--- config/prepare.sh
+++ config/prepare.sh
@@ -3,4 +3,9 @@ if ! [ -x "$(command -v 7z)" ]; then
     echo 'Error: you need 7z' >&2
     exit 1
 fi
+
+if [ -f "config.zip" ]; then
+    echo "config.zip already exists. Please remove it first." >&2
+    exit 1
+fi
 7z a -r config.zip palette.pal config.yaml rom.nes netplay-rom.nes linux/* macos/* windows/* 2>&1 >/dev/null
